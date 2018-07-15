d5ec4434644c44f6e54f274c9dfceb9f9f510347
conroy12345 <35980602+conroy12345@users.noreply.github.com>
2018-07-15 04:20:09 -0700

    Update ConstantsEnum.java

--- src/main/java/ConstantsEnum.java
+++ src/main/java/ConstantsEnum.java
@@ -1,1 +1,16 @@
 
+package org.shopping.constants;
+/*
+ * constants to use through the application
+ */
+public enum ConstantsEnum {
+	APPLE_COST("60"),ORANGE_COST("25"),APPLE("Apple"), ORANGE("Orange");
+String name;
+	private ConstantsEnum(String name) {
+	this.name=name;	
+	}
+	
+	public String toString() {
+		return name;
+	}
+}
